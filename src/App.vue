11ff689073a5b2946ff46f2ccc5ee74295dabb00
jsteacat <ubscoder@gmail.com>
2017-09-02 19:50:19 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Joke App!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
